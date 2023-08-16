37477befb0ce3faccf65b1a640682930a2a74d0e
Murtaza Nooruddin <murtaza@noorix.com>
2023-08-16 11:20:49 +1000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: [
+    './hugo_stats.json',
+    './assets/css/*.css'
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
